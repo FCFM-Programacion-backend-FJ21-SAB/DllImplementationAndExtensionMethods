be69f28a278650144e30650ac30ff9d40682e9d4
Rolando <rolando.fcfm2020@gmail.com>
2021-02-20 16:41:39 -0600

    First commit

---
+++ MyOwnOperations.cs
@@ -0,0 +1,20 @@
+﻿using FunctionsLibrary_DWB;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestFunctionDWB_Sabado
+{
+    public static class MyOwnOperations
+    {
+        public static decimal Div(this Function fn, decimal x, decimal y)
+        {
+            return x / y; 
+        }
+
+        public static decimal Resta(this Function f, decimal x, decimal y)
+        {
+            return x - y;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FunctionsLibrary_DWB;
+
+namespace TestFunctionDWB_Sabado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Function fn = new Function();
+            var x = fn.Sum(20, 43);
+            var y = fn.Mult(30, 12);
+            var w = fn.Div(10, 5);
+            var t = fn.Resta(100, 50);
+
+            Console.WriteLine("La suma es " + x);
+            Console.WriteLine("La multiplicación es " + y);
+
+            Console.WriteLine("La division es " + w);
+            Console.WriteLine("La resta es " + t);
+
+        }
+    }
+}
